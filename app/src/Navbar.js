df2c635b0e4372c3fa12c36bc8245aa43a0d08f9
Iram Lee <iram.lee@gmail.com>
2018-06-01 14:18:28 -0500

    create Navbar component and add it to page

---
+++ app/src/Navbar.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './Navbar.css';
+
+const Navbar = (props) => {
+    return(<div class="navbar">
+        <h1>Guess The Flag</h1>
+      </div>
+    );
+}
+
+export default Navbar;
