9a483a66815fd46969538552a17e904b1ab9f4b3
Iram Lee <iram.lee@gmail.com>
2018-06-06 12:11:48 -0500

    implement AJAX request and populate country options array

--- app/src/App.js
+++ app/src/App.js
@@ -10,12 +10,38 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      countryOptions : ['Canada', 'USA', 'UK', 'China'],
+      countryOptions : [],
       correctCountryName : '',
       flagURL : "https://restcountries.eu/data/alb.svg",
       buttonOption : 'GUESS'
     };
+    this.getOptions = this.getOptions.bind(this);
   }
+
+  componentDidMount(){
+    //make AJAX request to get all country/flag data
+    fetch('http://restcountries.eu/rest/v2/all')
+    .then(data => data.json())  //get 1st promise and wait 'till it resolves and THEN return the actual data (as json)
+    .then(countries => {        //here we now have the json data (inside countries)
+      const options = this.getOptions(countries);
+      this.setState({
+        countryOptions : options,
+      });
+    })
+    .catch((err) =>{
+      console.log(err);
+    });
+    //pick 4 random countries/flags and update state
+  }
+
+  getOptions(countries){
+    const arr = [];
+    for(let i = 0; i < 4; i++){
+      arr.push(countries[i].name);
+    }
+    return arr;
+  }
+
   render() {
     return (
       <div className="App">
@@ -24,7 +50,6 @@ class App extends Component {
           <OptionsBar countries = {this.state.countryOptions} />
           <GameButton buttonAction = {this.state.buttonOption} />
         </div>
-
         <Flag flagImgURL = {this.state.flagURL} />
       </div>
     );
